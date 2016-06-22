95f510192ee7dd82e200e335aeb10b6bf2882897
Youyou Han <youyou.han@zipscene.com>
2016-06-22 11:10:43 -0400

    added utils function test

--- test/utils.js
+++ test/utils.js
@@ -93,4 +93,45 @@ describe('utils', function() {
 			expect(result).to.equal(true);
 		});
 	});
+
+	describe('#genTokensForLongLatPoint', function() {
+		it('should return all the tokens for given point', function() {
+			let point = {
+				type: 'Point',
+				coordinates: [ 2, 3 ]
+			};
+			let steps = [ 256, 128, 64, 32, 16, 8, 4, 2 ];
+			let tokens = utils.genTokensForLongLatPoint(point, steps);
+			expect(tokens).to.be.an('array');
+			expect(tokens).to.have.length(9);
+			expect(tokens).to.include('0,256^256');
+			expect(tokens).to.include('0,128^128');
+			expect(tokens).to.include('0,64^64');
+			expect(tokens).to.include('0,32^32');
+			expect(tokens).to.include('0,16^16');
+			expect(tokens).to.include('0,8^8');
+			expect(tokens).to.include('0,4^4');
+			expect(tokens).to.include('2,4^2');
+			expect(tokens).to.include('0,4^2');
+
+			point = {
+				type: 'Point',
+				coordinates: [ 2, 2 ]
+			};
+			tokens = utils.genTokensForLongLatPoint(point, steps);
+			expect(tokens).to.be.an('array');
+			expect(tokens).to.have.length(11);
+			expect(tokens).to.include('0,256^256');
+			expect(tokens).to.include('0,128^128');
+			expect(tokens).to.include('0,64^64');
+			expect(tokens).to.include('0,32^32');
+			expect(tokens).to.include('0,16^16');
+			expect(tokens).to.include('0,8^8');
+			expect(tokens).to.include('0,4^4');
+			expect(tokens).to.include('2,4^2');
+			expect(tokens).to.include('2,2^2');
+			expect(tokens).to.include('0,4^2');
+			expect(tokens).to.include('0,2^2');
+		});
+	});
 });
