830e00f350041f33a6ff19cb280ea7110327aeab
Kevin <kevin.breneman@zipscene.com>
2016-06-24 11:40:35 -0400

    fixed circular reference issue

--- test/geometry.js
+++ test/geometry.js
@@ -542,4 +542,106 @@ describe('geometry', function() {
 			expect(geometry.squareLinearRingIntersectState(linearRing, topLeftVertex, size)).to.equal('intersect');
 		});
 	});
+
+	describe('isPointInsidePolygon', function() {
+		it('should return true for point inside polygon', function() {
+			let polygon = {
+				type: 'Polygon',
+				coordinates: [
+					[ [ 0, 0 ], [ 2, 0 ], [ 2, 2 ], [ 0, 2 ], [ 0, 0 ] ]
+				]
+			};
+			let point = [ 1, 1 ];
+			let result = geometry.isPointInsidePolygon(point, polygon);
+			expect(result).to.equal(true);
+		});
+
+		it('should return true for point inside exterior and outside all interiors', function() {
+			let polygon = {
+				type: 'Polygon',
+				coordinates: [
+					[ [ 0, 0 ], [ 4, 0 ], [ 4, 4 ], [ 0, 4 ], [ 0, 0 ] ],
+					[ [ 1, 1 ], [ 2, 1 ], [ 2, 2 ], [ 1, 2 ], [ 1, 1 ] ],
+					[ [ 3, 2 ], [ 3.5, 2 ], [ 3.5, 3 ], [ 3, 3 ], [ 3, 2 ] ]
+				]
+			};
+			let point = [ 2, 3 ];
+			let result = geometry.isPointInsidePolygon(point, polygon);
+			expect(result).to.equal(true);
+		});
+
+		it('should return false for point outside exterior', function() {
+			let polygon = {
+				type: 'Polygon',
+				coordinates: [
+					[ [ 0, 0 ], [ 2, 0 ], [ 2, 2 ], [ 0, 2 ], [ 0, 0 ] ]
+				]
+			};
+			let point = [ 5, 6 ];
+			let result = geometry.isPointInsidePolygon(point, polygon);
+			expect(result).to.equal(false);
+		});
+
+		it('should return false for point inside interior', function() {
+			let polygon = {
+				type: 'Polygon',
+				coordinates: [
+					[ [ 0, 0 ], [ 4, 0 ], [ 4, 4 ], [ 0, 4 ], [ 0, 0 ] ],
+					[ [ 1, 1 ], [ 2, 1 ], [ 2, 2 ], [ 1, 2 ], [ 1, 1 ] ],
+					[ [ 3, 2 ], [ 3.5, 2 ], [ 3.5, 3 ], [ 3, 3 ], [ 3, 2 ] ]
+				]
+			};
+			let point = [ 1.5, 1.5 ];
+			let result = geometry.isPointInsidePolygon(point, polygon);
+			expect(result).to.equal(false);
+		});
+
+		it('should return true for point lying on boundry of polygon', function() {
+			let polygon = {
+				type: 'Polygon',
+				coordinates: [
+					[ [ 0, 0 ], [ 4, 0 ], [ 4, 4 ], [ 0, 4 ], [ 0, 0 ] ],
+					[ [ 1, 1 ], [ 2, 1 ], [ 2, 2 ], [ 1, 2 ], [ 1, 1 ] ],
+					[ [ 3, 2 ], [ 3.5, 2 ], [ 3.5, 3 ], [ 3, 3 ], [ 3, 2 ] ]
+				]
+			};
+			let point = [ 3, 0 ];
+			let result = geometry.isPointInsidePolygon(point, polygon);
+			expect(result).to.equal(true);
+
+			point = [ 1.5, 1 ];
+			result = geometry.isPointInsidePolygon(point, polygon);
+			expect(result).to.equal(true);
+		});
+
+		it('should return true for point in MultiPolygon', function() {
+			let range = {
+				type: 'MultiPolygon',
+				coordinates: [ [
+					[ [ 0, 0 ], [ 2, 0 ], [ 2, 2 ], [ 0, 2 ], [ 0, 0 ] ],
+					[ [ 4, 4 ], [ 5, 4 ], [ 5, 5 ], [ 4, 5 ], [ 4, 4 ] ]
+				] ]
+			};
+			let point = [ 1, 1 ];
+			let result = geometry.isPointInsidePolygon(point, range);
+			expect(result).to.equal(true);
+
+			range = {
+				type: 'MultiPolygon',
+				coordinates: [
+					[
+						[ [ 0, 0 ], [ 8, 0 ], [ 8, 8 ], [ 0, 8 ], [ 0, 0 ] ],
+						[ [ 1, 1 ], [ 7, 1 ], [ 7, 7 ], [ 1, 7 ], [ 1, 1 ] ]
+					],
+					[
+						[ [ 2, 2 ], [ 4, 2 ], [ 4, 4 ], [ 2, 4 ], [ 2, 2 ] ]
+					]
+				]
+			};
+			point = [ 3, 3 ];
+			result = geometry.isPointInsidePolygon(point, range);
+			expect(result).to.equal(true);
+		});
+	});
+
 });
